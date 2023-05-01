421891170df4008c4bf7b95076499875f8344ab9
Jsim6342 <snowdrop6342@gmail.com>
2023-05-01 15:17:27 +0900

    5.1일자 학습_todolist 초안 개발

---
+++ JS강의/Todo-List/script.js
@@ -0,0 +1,56 @@
+const todoInput = document.getElementById("todo-input");
+const todoList = document.getElementById("todo-list");
+
+const createTodoList = () => {
+	const inputData = todoInput.value;
+
+	const newLi = document.createElement("li");
+	const newSpan = document.createElement("span");
+	const newBtn = document.createElement("button");
+
+	newBtn.addEventListener("click", () => {
+		newLi.classList.toggle("complete");
+	});
+
+	newLi.addEventListener("dblclick", () => {
+		newLi.remove();
+	});
+
+	newSpan.textContent = inputData;
+	newLi.appendChild(newBtn);
+	newLi.appendChild(newSpan);
+	todoList.appendChild(newLi);
+
+	todoInput.value = "";
+
+	saveItem();
+	console.log();
+};
+
+const checkKeyCode = () => {
+	if (window.event.keyCode === 13 && todoInput.value !== "") {
+		createTodoList();
+	}
+};
+
+const deleteAll = () => {
+	const liList = document.querySelectorAll("li");
+	for (let li of liList) {
+		li.remove();
+	}
+};
+
+const saveItem = () => {
+	const saveItems = [];
+
+	for (let li of todoList.children) {
+		const todoObj = {
+			contents: li.querySelector("span").textContent,
+			complete: li.classList.contains("complete"),
+		};
+
+		saveItems.push(todoObj);
+	}
+
+	console.log(saveItems);
+};
